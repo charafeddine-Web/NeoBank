d1eebb2efc618afabc92c07a23790b577dc9fc83
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-12-19 15:23:57 +0000

    NB-1: create user entity, repository, service and common exception

--- neobank-user/src/main/java/com/neobank/service/UserService.java
+++ neobank-user/src/main/java/com/neobank/service/UserService.java
@@ -1,4 +1,13 @@
 package com.neobank.service;
 
+import com.neobank.entity.User;
+
 public interface UserService {
+
+    User createUser(User user);
+
+    User getByUsername(String username);
+
+    User getById(Long id);
+
 }
